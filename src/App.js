106b3fb650d97974a5107c2056ed6af424fa9b7b
Pulkit <pulkitsingh071122@gmail.com>
2024-07-20 18:29:23 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Starting the mini login app task.
         </p>
         <a
           className="App-link"
